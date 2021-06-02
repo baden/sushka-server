c38c1f4ab07ea10ad883ef600ca2d1c4c9829da2
Denys Batrak <baden.i.ua@gmail.com>
2021-06-02 22:11:31 +0300

    Добавлен логотип в заголовок.

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,9 +1,12 @@
-
+import progress from './CryoVit-BLUE-transparent.png';
+console.log("progress", progress);
 
 export const fixLinks = (hwid) => {
     document.querySelector('a[href="/#"]').href += hwid;
     document.querySelector('a[href*="logs.html"]').href += hwid;
     document.querySelector('a[href*="series.html"]').href += hwid;
+
+    document.querySelector('header').style = `background-image: url("${progress}")`;
     // .href = "";
     // console.log("Logs: ", logs);
 
